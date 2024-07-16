4d13322b58ff043889f26567adb382258485a040
Pratyush ranjan <66966516+pratyushranjan115@users.noreply.github.com>
2024-07-16 17:16:52 +0530

    Update dealer.js

--- blocks/dealer/dealer.js
+++ blocks/dealer/dealer.js
@@ -21,7 +21,7 @@ export default function decorate(block) {
     
 
     const revealCheckbox = revealEl?.querySelector('input[type="checkbox"]');
-    const reveal = revealCheckbox ? revealCheckbox.checked : false;
+    const reveal = revealCheckbox ? revealCheckbox.checked : true;
 
     const href = hrefEl?.querySelector('a')?.href;
 
