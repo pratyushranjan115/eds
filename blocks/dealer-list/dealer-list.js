cfc4a724a5fc2cf0643886d9d656360ccf9cfc3d
Pratyush ranjan <66966516+pratyushranjan115@users.noreply.github.com>
2024-07-19 00:19:38 +0530

    Update dealer-list.js

--- blocks/dealer-list/dealer-list.js
+++ blocks/dealer-list/dealer-list.js
@@ -9,7 +9,7 @@ export default function decorate(block) {
         </div>
         <div class="dealer__content">
           <h3>${title}</h3>
-          <a href="${href}" class="dealer__link">Visit Dealer</a>
+          
         </div>
       </div>
     `;
@@ -24,24 +24,12 @@ export default function decorate(block) {
     `;
   }
 
-  function handleDealerClick(event) {
-    const dealerLink = event.currentTarget.querySelector('.dealer__link');
-    if (dealerLink) {
-      const href = dealerLink.getAttribute('href');
-      if (href) {
-        window.location.href = href; // Redirect to the specified href link
-      }
-    }
-  }
+  dealerCard.addEventListener('click', () => {
+    window.location.href = link;
+  });
 
   const dealerListData = block.dataset.dealerListData;
   const dealers = JSON.parse(dealerListData);
 
   block.innerHTML = renderDealerList(dealers);
-
-  // Add event listener to handle clicks on dealer components
-  const dealerElements = block.querySelectorAll('.dealer');
-  dealerElements.forEach((dealerElement) => {
-    dealerElement.addEventListener('click', handleDealerClick);
-  });
 }
