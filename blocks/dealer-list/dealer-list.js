50699e0ff7b83fec7f5feb2d138cef1ed98f8d8e
Pratyush ranjan <66966516+pratyushranjan115@users.noreply.github.com>
2024-07-19 14:23:06 +0530

    Update dealer-list.js

--- blocks/dealer-list/dealer-list.js
+++ blocks/dealer-list/dealer-list.js
@@ -29,14 +29,14 @@ console.log(block);
 
  
 
-//     block.innerHTML = `<div class="row">
-//     <div class="col-sm-12">
-//         <ul class="dealer-menu">
-//                ${dealersHTML}
-//         </ul>
-//     </div>
-// </div>
-// </div>`;
+    block.innerHTML = `<div class="row">
+    <div class="col-sm-12">
+        <ul class="dealer-menu">
+               ${dealersHTML}
+        </ul>
+    </div>
+</div>
+</div>`;
   
 
   // Any additional logic (like slider initialization) can be added here
