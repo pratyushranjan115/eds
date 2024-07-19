6b0e8558070c5059f5c995f23b4290ff27ed6123
Pratyush ranjan <66966516+pratyushranjan115@users.noreply.github.com>
2024-07-19 12:10:51 +0530

    aaa

--- blocks/dealer-list/dealer-list.js
+++ blocks/dealer-list/dealer-list.js
@@ -1,31 +1,50 @@
 export default function decorate(block) {
-  function renderDealer(dealerData) {
-    const { background_image, title, href } = dealerData;
+  function getDealerData(dealerElement) {
+    const backgroundImage = dealerElement.querySelector('[name="background_image"]')?.src || '';
+    const title = dealerElement.querySelector('[name="title"]')?.textContent?.trim() || '';
+    const href = dealerElement.querySelector('[name="href"]')?.href || '';
 
-    return `
-      <div class="dealer">
-        <div class="dealer__background">
-          <img src="${background_image}" alt="Background Image">
-        </div>
-        <div class="dealer__content">
-          <h3>${title}</h3>
-          <a href="${href}" class="dealer__link"></a>
-        </div>
+    return {
+      backgroundImage,
+      title,
+      href,
+    };
+  }
+
+  function renderDealer(dealerData) {
+    const { backgroundImage, title, href } = dealerData;
+
+    const dealerHTML = document.createElement('div');
+    dealerHTML.classList.add('dealer');
+
+    dealerHTML.innerHTML = `
+      <div class="dealer__background">
+        <img src="${backgroundImage}" alt="Background Image">
+      </div>
+      <div class="dealer__content">
+        <h3>${title}</h3>
+        <a href="${href}" class="dealer__link"></a>
       </div>
     `;
+    return dealerHTML;
   }
 
   function renderDealerList(dealers) {
-    const dealersHtml = dealers.map((dealerData) => renderDealer(dealerData));
-    return `
-      <div class="dealer-list">
-        ${dealersHtml.join('')}
-      </div>
-    `;
+    const dealerListElement = document.createElement('div');
+    dealerListElement.classList.add('dealer-list');
+
+    dealers.forEach(dealerData => {
+      const dealerElement = renderDealer(dealerData);
+      dealerListElement.appendChild(dealerElement);
+    });
+
+    return dealerListElement;
   }
 
-  const dealerListData = block.dataset.dealerListData;
-  const dealers = JSON.parse(dealerListData);
-
-  block.innerHTML = renderDealerList(dealers);
+  const dealerElements = block.querySelectorAll('.dealer');
+  const dealers = Array.from(dealerElements).map(getDealerData);
+  
+  const dealerListElement = renderDealerList(dealers);
+  block.innerHTML = '';
+  block.appendChild(dealerListElement);
 }
