a8c25d1396a2580840950e2e6259cab5618503ab
Pratyush ranjan <66966516+pratyushranjan115@users.noreply.github.com>
2024-07-19 16:38:26 +0530

    Update dealer-list.js

--- blocks/dealer-list/dealer-list.js
+++ blocks/dealer-list/dealer-list.js
@@ -1,57 +1,46 @@
 import utility from '../../utility/utility.js';
+
 export default function decorate(block) {
+  console.log(block);
 
+  const dealerListEl = [...block.children];
+  console.log(dealerListEl);
 
-console.log(block);
-
-
-  const [...dealerListEl]=block.children;
-   console.log(dealerListEl);
-  
   const dealersHTML = dealerListEl.map((dealer) => {
     const image = dealer.querySelector('picture');
-    
     const title = dealer.querySelector('p')?.textContent?.trim() || '';
     const href = dealer.querySelector('a')?.href || '';
 
-    if (image) {
-      const img = image.querySelector('img');
-      img.removeAttribute('width');
-      img.removeAttribute('height');
-  }
+    const imageHTML = image ? image.outerHTML : '';
 
-})
+    return `
+      <li>
+        <a href=${href}>
+          <div class="d-grid-item">
+            <div class="d-grid-item-icon">
+              ${imageHTML ? `<div class="feature__image">${imageHTML}</div>` : ''}
+            </div>
+            <div class="d-grid-item-title">
+              <h2>${title}</h2>
+            </div>
+          </div>
+        </a>
+      </li>
+    `;
+  }).join('');
 
-//     return `
-//     <li>
-//     <a href=${href} >
-//         <div class="d-grid-item">
-//             <div class="d-grid-item-icon">
-//                     ${(image) ? `<div class="feature__image">${image.outerHTML}</div>` : ''}
-//             </div>
+  const newHtml = `
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-sm-8 col-sm-10">
+          ${dealersHTML}
+        </div>
+      </div>
+    </div>
+  `;
 
-//             <div class="d-grid-item-title">
-//                 <h2>${title}</h2>
-//             </div>
-//         </div>
-//     </a>
-// </li>
-//   `;
-//   }).join('');
-
- 
-//     const newHtml= `<div class="row">
-//     <div class="col-sm-12">
-//         <ul class="dealer-menu">
-//                ${dealersHTML}
-//         </ul>
-//     </div>
-// </div>
-// </div>`
-//     block.innerHTML = '';
-//     block.insertAdjacentHTML('beforeend', utility.sanitizeHtml(newHtml));
-
-  
+  block.innerHTML = '';
+  block.insertAdjacentHTML('beforeend', utility.sanitizeHtml(newHtml));
 
   // Any additional logic (like slider initialization) can be added here
 }
