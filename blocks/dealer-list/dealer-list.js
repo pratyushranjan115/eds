54af66d7cf68f5325f2906d9b1a030c74d962692
Pratyush ranjan <66966516+pratyushranjan115@users.noreply.github.com>
2024-07-19 12:22:24 +0530

    Update dealer-list.js

--- blocks/dealer-list/dealer-list.js
+++ blocks/dealer-list/dealer-list.js
@@ -1,28 +1,28 @@
-export default function decorate(block) {
-  function renderDealer(dealerData) {
-    const { background_image, title, href } = dealerData;
+// export default function decorate(block) {
+//   function renderDealer(dealerData) {
+//     const { background_image, title, href } = dealerData;
 
-    return `
-      <div class="dealer">
-        <div class="dealer__background">
-          <img src="${background_image}" alt="Background Image">
-        </div>
+//     return `
+//       <div class="dealer">
+//         <div class="dealer__background">
+//           <img src="${background_image}" alt="Background Image">
+//         </div>
        
-      </div>
-    `;
-  }
+//       </div>
+//     `;
+//   }
 
-  function renderDealerList(dealers) {
-    const dealersHtml = dealers.map((dealerData) => renderDealer(dealerData));
-    return `
-      <div class="dealer-list">
-        ${dealersHtml.join('')}
-      </div>
-    `;
-  }
+//   function renderDealerList(dealers) {
+//     const dealersHtml = dealers.map((dealerData) => renderDealer(dealerData));
+//     return `
+//       <div class="dealer-list">
+//         ${dealersHtml.join('')}
+//       </div>
+//     `;
+//   }
 
-  const dealerListData = block.dataset.dealerListData;
-  const dealers = JSON.parse(dealerListData);
+//   const dealerListData = block.dataset.dealerListData;
+//   const dealers = JSON.parse(dealerListData);
 
-  block.innerHTML = renderDealerList(dealers);
-}
+//   block.innerHTML = renderDealerList(dealers);
+// }
