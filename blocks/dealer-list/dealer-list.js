74bb128974c39d45b60942549a75ba930c56f42f
Pratyush ranjan <66966516+pratyushranjan115@users.noreply.github.com>
2024-07-19 15:12:01 +0530

    Update dealer-list.js

--- blocks/dealer-list/dealer-list.js
+++ blocks/dealer-list/dealer-list.js
@@ -5,38 +5,39 @@ console.log(block);
 
 
    const dealerElements = Array.from(block.children);
+   console.log(block.children);
   
-  const dealersHTML = dealerElements.map(dealer => {
-    const backgroundImage = dealer.querySelector('img')?.src || '';
-    const title = dealer.querySelector('p')?.textContent?.trim() || '';
-    const href = dealer.querySelector('a')?.href || '';
-    return `
-    <li>
-    <a href=${href} >
-        <div class="d-grid-item">
-            <div class="d-grid-item-icon">
-                    <img src=${backgroundImage} alt="" title="New Finance Journey">
-            </div>
+//   const dealersHTML = dealerElements.map(dealer => {
+//     const backgroundImage = dealer.querySelector('img')?.src || '';
+//     const title = dealer.querySelector('p')?.textContent?.trim() || '';
+//     const href = dealer.querySelector('a')?.href || '';
+//     return `
+//     <li>
+//     <a href=${href} >
+//         <div class="d-grid-item">
+//             <div class="d-grid-item-icon">
+//                     <img src=${backgroundImage} alt="" title="New Finance Journey">
+//             </div>
 
-            <div class="d-grid-item-title">
-                <h2>${title}</h2>
-            </div>
-        </div>
-    </a>
-</li>
-  `;
-  }).join('');
+//             <div class="d-grid-item-title">
+//                 <h2>${title}</h2>
+//             </div>
+//         </div>
+//     </a>
+// </li>
+//   `;
+//   }).join('');
 
  
 
-    block.innerHTML = `<div class="row">
-    <div class="col-sm-12">
-        <ul class="dealer-menu">
-               ${dealersHTML}
-        </ul>
-    </div>
-</div>
-</div>`;
+//     block.innerHTML = `<div class="row">
+//     <div class="col-sm-12">
+//         <ul class="dealer-menu">
+//                ${dealersHTML}
+//         </ul>
+//     </div>
+// </div>
+// </div>`;
   
 
   // Any additional logic (like slider initialization) can be added here
