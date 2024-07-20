d03526a71d82dc9010e206c66887e361fa268096
Pratyush ranjan <66966516+pratyushranjan115@users.noreply.github.com>
2024-07-20 17:10:23 +0530

    sds

--- blocks/dealer-list/dealer-list.js
+++ blocks/dealer-list/dealer-list.js
@@ -1,5 +1,6 @@
 import utility from '../../utility/utility.js';
 import ctaUtils from '../../utility/ctaUtils.js';
+
 export default function decorate(block) {
 
   function initImage(image, altTextEl) {
@@ -9,7 +10,7 @@ export default function decorate(block) {
     const alt = altTextEl?.textContent?.trim() || 'image';
     img.setAttribute('alt', alt);
   }
-  
+
   const cards = [...block.children]
     .map((child) => {
       const [
@@ -18,64 +19,41 @@ export default function decorate(block) {
         titleEl,
         ctaLinkEl
       ] = child.children;
-      
-  
+
       const backgroundImage = backgroundImageEl?.querySelector('picture');
       if (backgroundImage) {
         initImage(backgroundImage, backgroundAltTextEl);
       }
-  
-     
-  
+
       const title = titleEl?.textContent?.trim();
-      console.log(title);
       const primaryCta = ctaUtils.getLink(
         ctaLinkEl,
         '',
         '__blank',
         'primary__btn',
       );
-      let ctaHtml = '';
-      if (primaryCta) {
-        ctaHtml = `
-          <div class="dealer__actions">
-            ${primaryCta ? primaryCta.outerHTML : ''}
-          </div>
-        `;
-      }
-      if (title) {
-        
-        // title.classList.add('dealer__title');
-       
 
-      }
-      console.log(primaryCta);
       child.innerHTML = '';
-      const link = primaryCta.href;
-      console.log(link);
+      const link = primaryCta?.href || '#';
       child.insertAdjacentHTML(
         'beforeend',
         utility.sanitizeHtml(`
-          <a href=${link}>
-          ${backgroundImage ? `<div class="dealer__backgroundImage">${backgroundImage.outerHTML}</div>` : ''}
-          <div class="dealer__content">
-            <div class="dealer__info">
-            ${title ? `<div class="dealer__title"><h2>${title}</h2></div>` : ''}
-            </div>
-          </div>
-          </a>
+          <li>
+            <a href="${link}">
+              ${backgroundImage ? `<div class="d-grid-item-icon">${backgroundImage.outerHTML}</div>` : ''}
+              <div class="d-grid-item">
+                ${title ? `<div class="d-grid-item-title"><h2>${title}</h2></div>` : ''}
+              </div>
+            </a>
+          </li>
         `),
       );
-      child.classList.add('dealer__card');
       return child.outerHTML;
     })
     .join('');
-  block.innerHTML = '';
-  block.insertAdjacentHTML('beforeend', utility.sanitizeHtml(cards));
 
-
-  console.log(block);
-
-
-
+  block.innerHTML = `<ul class="dealer-menu">${cards}</ul>`;
+  block.classList.add('grey-bg');
 }
+
+// Assuming your CSS is in a separate file, make sure it is properly imported and applied.
